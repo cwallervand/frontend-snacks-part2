6511f41eeae49df8e7888082aed89594483855cc
Christian Wallervand <cwallervand@gmail.com>
2024-11-05 14:02:55 +0100

    Added search element and started css layers

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.css';
+
+const findOnPageSerrchButton = document.querySelector(
+  '#findOnPageSearch button'
+);
+const findOnPageInput = document.querySelector('#findOnPageInput');
+
+findOnPageInput.addEventListener('keypress', (event) => {
+  console.log(event.key);
+  if (event.key.toLowerCase() === 'enter') {
+    findOnPage();
+  }
+});
+
+function findOnPage() {
+  const searchValue = findOnPageInput.value;
+  findOnPageInput.value = '';
+  if (searchValue) {
+    location.replace(`/#:~:text=${searchValue}`);
+  }
+}
+
+findOnPageSerrchButton.addEventListener('click', () => {
+  findOnPage();
+});
